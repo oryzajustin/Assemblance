e921282af307b0f7d1bddbae1e07b72df03c81e9
YuanC <jerrych.97@gmail.com>
2020-03-06 22:41:30 -0500

    swaggy

---
+++ Assets/DimensionalTrigger/DimensionalTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionalTrigger : MonoBehaviour
+{
+    public int Dimension;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InterdimensionalObject hitObject = other.GetComponent<InterdimensionalObject>();
+        if (hitObject != null)
+        {
+            hitObject.UpdateCurrentDimension(Dimension);
+        }
+    }
+}
---
+++ Assets/Key/Key.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : HoldableItem
+{
+    public Door AccompanyingDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
