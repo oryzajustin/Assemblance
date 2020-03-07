896dfa541eda69ead95d4d1b1047238e4b42e49d
YuanC <jerrych.97@gmail.com>
2020-03-07 18:35:26 -0500

    new fogs

--- Assets/Scripts/CameraSys/MasterCamera.cs
+++ Assets/Scripts/CameraSys/MasterCamera.cs
@@ -8,29 +8,64 @@ public class MasterCamera : MonoBehaviour {
     public GameObject FogRight;
     public ParticleSystem FogParticles;
 
+    public bool FogEnabled = false;
+    public float AspectRatio;
+
     void Awake() {
         SetFogSide("left");
     }
 
-    public void SetFogSide(string side)
+    void Start()
     {
-        if (side == "left")
+        Camera camera = GetComponentInChildren<Camera>();
+        camera.aspect = AspectRatio;
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+
+        if (AspectRatio > windowAspect) // Bars on top/bottom
         {
-            FogLeft.SetActive(true);
-            FogRight.SetActive(false);
+            float diff = 1 - (windowAspect / AspectRatio);
+            camera.rect = new Rect(0, (diff / 2.0f), 1f, 1.0f - diff);
+
         }
-        else
+        else // AspectRatio < windowAspect, Columns on side
+        {
+            float diff = 1 - (AspectRatio / windowAspect);
+            camera.rect = new Rect((diff / 2.0f), 0, (1.0f - diff), 1f);
+        }
+
+        if (!FogEnabled)
         {
             FogLeft.SetActive(false);
-            FogRight.SetActive(true);
+            FogRight.SetActive(false);
+            FogParticles.Stop();
         }
-        FogParticles.Play();
+    }
+
+    public void SetFogSide(string side)
+    {
+        if (FogEnabled)
+        {
+            if (side == "left")
+            {
+                FogLeft.SetActive(true);
+                FogRight.SetActive(false);
+            }
+            else
+            {
+                FogLeft.SetActive(false);
+                FogRight.SetActive(true);
+            }
+            FogParticles.Play();
+        }
     }
 
     public void RemoveFog()
     {
-        FogLeft.SetActive(false);
-        FogRight.SetActive(false);
-        FogParticles.Stop();
+        if (FogEnabled)
+        {
+            FogLeft.SetActive(false);
+            FogRight.SetActive(false);
+            FogParticles.Stop();
+        }
     }
 }
