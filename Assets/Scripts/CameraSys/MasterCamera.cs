28af9cfcadba58c82c5ab8cc08c0d972bcf06a90
YuanC <jerrych.97@gmail.com>
2020-03-07 15:01:52 -0500

    fog shaders

--- Assets/Scripts/CameraSys/MasterCamera.cs
+++ Assets/Scripts/CameraSys/MasterCamera.cs
@@ -4,45 +4,23 @@ using UnityEngine;
 
 public class MasterCamera : MonoBehaviour {
 
-    public GameObject fog;
-    public GameObject planeA;
-    public GameObject planeB;
+    public GameObject FogLeft;
+    public GameObject FogRight;
+    public GameObject FogParticles;
 
-    private GameObject localFog;
-
-    void Start() {
-        fogInit();
-        enableFogForPlaneB();
+    void Awake() {
+        SetFog("left");
     }
 
-    public void fogInit() {
-        localFog = Instantiate(fog, fog.transform.localPosition, Quaternion.Euler(0, 90f, 0)) as GameObject;
+    public void SetFog(string side)
+    {
+        if (side == "left")
+        {
+
+        }
+        else
+        {
+
+        }
     }
-
-    private void enableFog(GameObject plane) {
-        localFog.transform.parent = plane.transform;
-        localFog.transform.localPosition = new Vector3(0, 0, 0);
-    }
-
-    public void disableFog(GameObject plane) {
-        localFog.transform.parent = null;
-    }
-
-    public void enableFogForPlaneA() {
-        enableFog(planeA);
-    }
-
-    public void enableFogForPlaneB() {
-        enableFog(planeB);
-    }
-
-    public void switchToPlayerA() {
-        enableFogForPlaneB();
-    }
-
-    public void switchToPlayerB() {
-        enableFogForPlaneA();
-    }
-
-
 }
