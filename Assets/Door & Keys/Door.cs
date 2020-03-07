9cbb2a69e2c4bb7abc7869cf67fb8ccd1089d7c2
savkhan <savish.khan@gmail.com>
2020-03-07 14:59:23 -0500

    Door & Keys

---
+++ Assets/Door & Keys/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Photon.Pun;
+
+public class Door : MonoBehaviourPun
+{
+    public GameObject accompanyingKey;
+
+    [PunRPC] public void DestroyDoor() {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On Trigger Enter");
+        Key key = other.GetComponent<Key>();
+        if (key != null) {
+            Debug.Log("Key is not null");
+            if (key.AccompanyingDoor == this) {
+                Debug.Log("Key matches accompanying door");
+                // Match
+                //Destroy(gameObject);
+                if (PhotonNetwork.IsMasterClient) {
+                    photonView.RPC("DestroyDoor", RpcTarget.All);
+                }
+            }
+        }
+    }
+
+}
